964c6f1152462d168ec7ae38f62eb27c9ed3782f
kanonundgigue <kanon@hydra.t.u-tokyo.ac.jp>
2025-02-22 17:39:03 +0900

    sensitivity testts have been done

--- analysis/param_test.py
+++ analysis/param_test.py
@@ -11,8 +11,7 @@ def set_sensitive_param(ISO_TYPE: str = "HDO"):
     return{
             "resub_factor": [0.2, 0.5],    
             "prcp_duration":[0.25, 1, 3], 
-            "CLOUD_AGING": [True, False],
-            "delta_q_surf": [-120, -100] if ISO_TYPE == "HDO" else [-15, -12.5],
+            # "CLOUD_AGING": [True, False],
             "temp_air_fin": [-20, -10]
     }
     
@@ -20,14 +19,15 @@ def set_test_param(ISO_TYPE: str = "HDO"):
     return{
             # Post precipitation
             "prcp_perday":[1, 2],
-            "p_btm": [700, 850],
             "p_top": [400, 500],
+            # "CLOUD_AGING": [True, False],
            #  # Background surface condition
             "rh_surf": [0.7, 0.8],
             # Rayleigh distillation
             "dt": [0.5, 1],
             "ALPHA_RY_MODE": ["eq", "eff"], 
            #  # Initial evaporation
+            # "delta_q_surf": [-120, -100] if ISO_TYPE == "HDO" else [-15, -12.5],
             "h_air": [1, 0.5],
             "surface_wind": [6.5, 35],
             "temp_air_init_list": [[5, 10, 15], [0, 5, 10]], 
@@ -37,14 +37,15 @@ def set_fix_param(ISO_TYPE: str = "HDO"):
             return {
             # Post precipitation
             "prcp_perday":[2],
-            "p_btm": [700],
-            "p_top": [400],
+            "p_top": [500],
+            # "CLOUD_AGING": [False],
            #  # Background surface condition
             "rh_surf": [0.75],
             # Rayleigh distillation
             "dt": [0.5],
             "ALPHA_RY_MODE": ["eff"], 
            #  # Initial evaporation
+            # "delta_q_surf": [-120] if ISO_TYPE == "HDO" else [-15],
             "h_air": [1],
             "surface_wind": [6.5],
             "temp_air_init_list": [[5, 10, 15]],
